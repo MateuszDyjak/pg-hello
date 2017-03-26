00e80316d5718f338dbe6c2ab14e301ae655e923
MateuszDyjak <mateuszdyjak5@gmail.com>
2017-03-26 16:52:30 +0200

    New button added

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -10,13 +10,28 @@ function deviceInfo() {
 
 	info =  'Hi, I am your smartphone :-)' + '\n' +
 			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
+			'Device Name    : '     + device.name     + '\n' +
+			'Device Cordova : '  + device.cordova + '\n' +
+			'Device Platform: ' + device.platform + '\n' +
+			'Device UUID    : '     + device.uuid     + '\n' +
+			'Device Model   : '    + device.model     + '\n' +
 			'Device Version : '  + device.version  + '\n';
 
+
 	navigator.notification.alert(info);
-	
+
+}
+function displayMyData() {
+
+	info= 'Hello! Below you can find some my personal data \n'
+
+	+ 'Name : Mateusz\n'
+	+ 'Last name : Dyjak\n'
+	+ 'City : Kraków\n'
+	+ 'Settlement : Dąbie\n'
+	+ 'District : Grzegórzki'
+	+ 'email address : mateuszdyjak5@gmail.com' ;
+
+navigator.notification.alert(info);
+
 }
